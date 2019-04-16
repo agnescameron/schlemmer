5b16fc259834d66f7c89837e8c104786094f1bf5
agnescameron <agnesfcameron@gmail.com>
2019-04-16 13:10:12 -0400

    aaa

--- server/server.js
+++ server/server.js
@@ -21,16 +21,37 @@ app.get('/', function(req, res){
  
 
 var WebSocketServer = require('ws').Server,
-wss = new WebSocketServer({port: 40510})
+wss1 = new WebSocketServer({port: 40510})
+wss2 = new WebSocketServer({port: 40511})
 
-wss.on('connection', function (ws) {
-	wss.clients.forEach(function(client){
+
+wss1.on('connection', function (ws) {
+	wss1.clients.forEach(function(client){
 	  ws.on('message', function (message) {
 	    console.log('received: %s', message)
 	  })
 	  setInterval(
 	    () => { 
-		  fs.readFile('./data.txt', function(err, data) {
+		  fs.readFile('./data1.txt', function(err, data) {
+			   	try{
+			    	ws.send(`${data}`)
+			   	}
+			    catch(err){
+			    	// console.log('sorry babe')
+			    }
+			});
+		  });
+	   }, 100)
+})
+
+wss2.on('connection', function (ws) {
+	wss2.clients.forEach(function(client){
+	  ws.on('message', function (message) {
+	    console.log('received: %s', message)
+	  })
+	  setInterval(
+	    () => { 
+		  fs.readFile('./data2.txt', function(err, data) {
 			   	try{
 			    	ws.send(`${data}`)
 			   	}
