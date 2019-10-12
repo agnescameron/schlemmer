c58634072ae0ef82743010c80fe76748aba3733b
agnescameron <agnesfcameron@gmail.com>
2019-10-12 19:07:42 -0400

    minimal threading file

--- server/uart-tests/threads.py
+++ server/uart-tests/threads.py
@@ -8,28 +8,24 @@ pg.init()
 pg.mixer.set_num_channels(50)
 
 
-drum0 = pg.mixer.Sound("../../sounds/japanese-percussion/biz.wav")
-drum1 = pg.mixer.Sound("../../sounds/japanese-percussion/taiko.wav")
+
+drum1 = pg.mixer.Sound("../../sounds/japanese-percussion/biz.wav")
+drum4 = pg.mixer.Sound("../../sounds/japanese-percussion/taiko.wav")
 drum2 = pg.mixer.Sound("../../sounds/japanese-percussion/hyoshigi.wav")
 drum3 = pg.mixer.Sound("../../sounds/japanese-percussion/kagura.wav")
-drums = [drum0, drum1, drum2, drum3]
- 
-def thread_function(num):
-	logging.info("Thread %s: starting", num)
+drum0 = pg.mixer.Sound("../../sounds/japanese-percussion/tsuzumi.wav")
+drums = [drum0, drum1, drum2, drum3, drum4]
+
+
+def channel(num, pause):
 	while True:
 		drums[num].play()
-		time.sleep(num)
-	logging.info("Thread %s: finishing", num)
+		time.sleep(num+0.2)
 
 if __name__ == "__main__":
-	format = "%(asctime)s: %(message)s"
-	logging.basicConfig(format=format, level=logging.INFO,
-	                 datefmt="%H:%M:%S")
+	thread = threading.Thread(target=printFile, args=(0, ))
+	thread.start()
 
-	logging.info("Main    : before creating thread")
 	for i in range (0, len(drums)):
-		thread = threading.Thread(target=thread_function, args=(i,))
-		thread.start()		
-	logging.info("Main    : wait for the thread to finish")
-	# x.join()
-	logging.info("Main    : all done")
+		thread = threading.Thread(target=channel, args=(i, i))
+		thread.start()
