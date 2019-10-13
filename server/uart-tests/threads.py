d7a71640918acd69814e74c8c5c401790592118f
agnescameron <agnesfcameron@gmail.com>
2019-10-13 12:17:38 -0400

    reading from file and sharing between threads

--- server/uart-tests/threads.py
+++ server/uart-tests/threads.py
@@ -1,7 +1,9 @@
 import logging
 import threading
 import pygame as pg
+import math
 import time
+import re
 
 pg.mixer.init()
 pg.init()
@@ -15,14 +17,23 @@ drum2 = pg.mixer.Sound("../../sounds/japanese-percussion/hyoshigi.wav")
 drum3 = pg.mixer.Sound("../../sounds/japanese-percussion/kagura.wav")
 drum0 = pg.mixer.Sound("../../sounds/japanese-percussion/tsuzumi.wav")
 drums = [drum0, drum1, drum2, drum3, drum4]
+sleep = [1, 1, 1, 1, 1]
 
+def printFile(arg):
+	file = open("../accelo-flow-data1.txt", 'r')
+	for line in file:
+		data = line.split()
+		sleep[int(data[3])] = math.sqrt( int(data[0])**2 + int(data[1])**2 + int(data[2])**2 )
+		time.sleep(0.05)
 
 def channel(num, pause):
 	while True:
 		drums[num].play()
+		print(sleep[num])
 		time.sleep(num+0.2)
 
 if __name__ == "__main__":
+
 	thread = threading.Thread(target=printFile, args=(0, ))
 	thread.start()
 
